1572f17b928968e333c460506e903cc6e645cb68
AdrienVerwaerde <verwaerdeadrien@gmail.com>
2025-05-06 15:04:35 +0200

    final touch

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, Button, Typography, useMediaQuery } from '@mui/material'
+import { AppBar, Box, Button, Stack, Typography, useMediaQuery } from '@mui/material'
 import jungle from '../../assets/jungle.jpg'
 
 
@@ -9,9 +9,12 @@ const Footer = () => {
         <Box>
             <AppBar position="static" sx={{ width: '100%', backgroundColor: 'transparent', backgroundImage: `url(${jungle})` }}>
                 <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: 2 }}>
-                    <Button onClick={() => window.scrollTo(0, 0)} sx={{ p: 0 }}>
-                        <img src="/dinologo.png" alt="logo" width="70" />
-                    </Button>
+                    <Stack direction="row">
+                        <Button onClick={() => window.scrollTo(0, 0)} sx={{ p: 0 }}>
+                            <img src="/dinologo.png" alt="logo" width="70" />
+                        </Button>
+                        <Typography sx={{ fontFamily: "Roboto Slab", fontSize: isMobile ? '12px' : '24px', color: 'white', textShadow: '1px 1px 4px #000000', mt: 0.5,ml: isMobile ? 0.2 : 0.5, textAlign: 'left' }}>click me to go back to top</Typography>
+                    </Stack>
                     <Typography sx={{ fontFamily: "Rubik Distressed", fontSize: isMobile ? '20px' : '30px', color: 'white', textShadow: '1px 1px 4px #000000', mr: 2 }}>
                         FÉRIEVAURUS
                     </Typography>
